786ba29493707fd15f81aba62fa45344864bff90
ZEESHAN KHAN <162082741+ZEESHAN8692@users.noreply.github.com>
2025-08-24 00:49:34 +0530

    asdsad

--- app/middleware/authCheck.js
+++ app/middleware/authCheck.js
@@ -18,3 +18,29 @@ const AuthCheck = (req, res, next) => {
   }
   return next();
 };
+
+
+const adminCheck = (req, res, next) => {
+  const token =
+    req?.body?.token || req?.query?.token || req?.headers["x-access-token"];
+  if (!token) {
+    return res.status(400).json({
+      message: "Token is required for access this page",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET_KEY);
+    req.user = decoded;
+    console.log("afetr login data", req.user);
+    if (req.user.role !== "admin") {
+      return res.status(400).json({
+        message: "You are not admin",
+      });
+    }
+  } catch (err) {
+    return res.status(400).json({
+      message: "Invalid token",
+    });
+  }
+  return next();
+};
